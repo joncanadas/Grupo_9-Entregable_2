ee196863d495ad9dafb67c9db5aa4d6a94e90d28
Mikel García <garcia.mikel@opendeusto.es>
2024-05-01 13:44:09 +0200

    arreglado de errores, ya sale el html

--- Proyectos/EmpresaProyectoGestion/DjangoGestionProyectos/DjangoGePrApp/views.py
+++ Proyectos/EmpresaProyectoGestion/DjangoGestionProyectos/DjangoGePrApp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'DjangoGePrApp/index.html')
+    return render(request, 'index.html')
 
 
