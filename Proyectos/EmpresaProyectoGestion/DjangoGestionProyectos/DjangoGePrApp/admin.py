267a9eb5f269e86eafe87c56a30e398acc0e34e2
Jon Cañadas <jon.canadas@opendeusto.es>
2024-05-01 17:54:57 +0200

    + Codigo Proyecto

--- Proyectos/EmpresaProyectoGestion/DjangoGestionProyectos/DjangoGePrApp/admin.py
+++ Proyectos/EmpresaProyectoGestion/DjangoGestionProyectos/DjangoGePrApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Empleado, Proyecto, Tarea, Participa, Encarga
+ 
 # Register your models here.
+
+admin.site.register(Empleado)
+admin.site.register(Proyecto)
+admin.site.register(Tarea)
+admin.site.register(Participa)
+admin.site.register(Encarga)
