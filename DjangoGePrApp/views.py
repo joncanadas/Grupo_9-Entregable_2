9062e49f8c463571b69e985c198eb345425b82be
Jon Cañadas <jon.canadas@opendeusto.es>
2024-05-02 16:48:15 +0200

    +Codigo Html

--- DjangoGePrApp/views.py
+++ DjangoGePrApp/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Proyecto
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    proyectos = Proyecto.objects.all
+    context = {'lista_proyectos' : proyectos}
+    return render(request, 'index.html', context)
 
 
+def listadoProyectos(request):
+    proyectos = Proyecto.objects.all
+    context = {'titulo_pagina' : 'Listado de Proyectos', 'lista_poryectos' : proyectos}
+    return render(request, 'listadoProyectos.html', context)
