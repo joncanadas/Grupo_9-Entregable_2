34826dead81e464654e50c5277b361549f8ee26d
Jon Cañadas <jon.canadas@opendeusto.es>
2024-05-03 18:24:26 +0200

    +Codigo Html

--- DjangoGePrApp/urls.py
+++ DjangoGePrApp/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('crearProyecto/', views.crearProyecto, name='crearProyecto'),
     path('listados/', views.listados, name='listados'),
-    path('modificarProyecto/<int:proyecto_id>/', views.modificar_Proyecto, name='modificarProyecto'),
+    path('modificarProyecto/<int:proyecto_id>/', views.modificarProyecto, name='modificarProyecto'),
     path('crearTarea/', views.crearTarea, name='crearTareas'),
+    path('proyecto/delete/<int:proyecto_id>/', views.borrarProyecto, name='borrarProyecto'),
+    path('tarea/delete/<int:tarea_id>/', views.borrarTarea, name='borrarTarea'),
+
 ]
--- DjangoGePrApp/views.py
+++ DjangoGePrApp/views.py
@@ -17,18 +17,33 @@ def listados(request):
     context = {'titulo_pagina' : 'Listados de Proyectos y Tareas', 'lista_proyectos' : proyectos, 'lista_tareas' : tareas}
     return render(request, 'listados.html', context)
 
-def modificar_Proyecto(request, proyecto_id):
-    proyecto = Proyecto.objects.get(pk=proyecto_id)    
-
-    Descripcion = request.POST["DescripcionProyectoMod"]
-    Fecha_Inicio = request.POST["FechaInicioMod"]
-    Fecha_Fin = request.POST["FechaFinMod"]
-    Presupuesto = request.POST["PresupuestoMod"]
+def modificarProyecto(request, proyecto_id):
+    proyecto = Proyecto.objects.get(pk=proyecto_id)
+    form_Proyecto = modelform_factory(Proyecto)
+    
+    if request.method == 'POST':
+        form = form_Proyecto(request.POST, instance=proyecto)
         
-    context = {'titulo_pagina' : 'Modificar Proyectos', 'proyecto': proyecto}
+        if form.is_valid():
+            form.save()
+            return redirect('detalle_proyecto', pk=proyecto.pk)
+    else:
+        form = modelform_factory(instance=proyecto)
+        
+    context = {'titulo_pagina' : 'Modificar Proyectos', 'form': form, 'proyecto': proyecto}
     return render(request, 'modificarProyecto.html', context)
 
 def crearTarea(request):
     tareas = Tarea.objects.all
     context = {'titulo_pagina' : 'Crear Tareas', 'lista_tareas' : tareas}
     return render(request, 'crearTarea.html', context)
+
+def borrarProyecto(request, proyecto_id):
+    proyecto = Proyecto.objects.get(pk=proyecto_id)
+    proyecto.delete()
+    return redirect('listados')
+
+def borrarTarea(request, tarea_id):
+    proyecto = Tarea.objects.get(pk=tarea_id)
+    proyecto.delete()
+    return redirect('listados')
