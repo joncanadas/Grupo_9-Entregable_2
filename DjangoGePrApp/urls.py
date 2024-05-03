b899ac57ea9565b0295e4ee97ebce9799c40af30
Jon Cañadas <jon.canadas@opendeusto.es>
2024-05-03 16:00:20 +0200

    +Codigo Html

--- DjangoGePrApp/urls.py
+++ DjangoGePrApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('crearProyecto/', views.crearProyecto, name='crearProyecto'),
     path('listados/', views.listados, name='listados'),
-    path('modificarProyecto/', views.modificarProyectos, name='modificarProyectos'),
+    path('modificarProyecto/<int:proyecto_id>/', views.modificarProyecto, name='modificarProyecto'),
     path('crearTarea/', views.crearTarea, name='crearTareas'),
 ]
--- DjangoGePrApp/views.py
+++ DjangoGePrApp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.forms import modelform_factory
 from .models import Proyecto, Tarea
 
 # Create your views here.
@@ -15,9 +17,20 @@ def listados(request):
     context = {'titulo_pagina' : 'Listados de Proyectos y Tareas', 'lista_proyectos' : proyectos, 'lista_tareas' : tareas}
     return render(request, 'listados.html', context)
 
-def modificarProyectos(request):
-    proyectos = Proyecto.objects.all
-    context = {'titulo_pagina' : 'Modificar Proyectos', 'lista_proyectos' : proyectos}
+def modificarProyecto(request, proyecto_id):
+    proyecto = Proyecto.objects.get(pk=proyecto_id)
+    form_Proyecto = modelform_factory(Proyecto)
+    
+    if request.method == 'POST':
+        form = form_Proyecto(request.POST, instance=proyecto)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('detalle_proyecto', pk=proyecto.pk)
+    else:
+        form = modelform_factory(instance=proyecto)
+        
+    context = {'titulo_pagina' : 'Modificar Proyectos', 'form': form, 'proyecto': proyecto}
     return render(request, 'modificarProyecto.html', context)
 
 def crearTarea(request):
