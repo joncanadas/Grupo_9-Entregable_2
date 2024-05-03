95c4860c7c4740fca262b2a1a047e8a3c4a3107d
Mikel García <garcia.mikel@opendeusto.es>
2024-05-03 18:55:16 +0200

    conflictos arreglados

--- DjangoGePrApp/forms.py
+++ DjangoGePrApp/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.forms import ModelForm
 from .models import Proyecto, Tarea
 
@@ -11,15 +10,3 @@ class TareaForm(ModelForm):
     class Meta:
         model = Tarea
         fields = '__all__'
-=======
-from django import forms
-from DjangoGePrApp.models import Proyecto, Tarea
-
-
-class ProyectoForm(forms.Form):
-    Nombre = forms.CharField(label="Nombre", max_length=100)
-    Descripcion = forms.CharField(label="Descripcion", max_length=300)
-    Fecha_Inicio = forms.DateField(label="Fecha_Inicio")
-    Fecha_Fin = forms.DateField(label="Fecha_Fin")
-    Presupuesto = forms.IntegerField(label="Presupuesto")
->>>>>>> cd34b82c221bfcacbaa77beb733ef12fbbff4334
