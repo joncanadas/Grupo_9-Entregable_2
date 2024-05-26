5446c5ee9a3f8a6eb6e90901c51eb70317584b1d
Mikel García <garcia.mikel@opendeusto.es>
2024-05-26 13:54:40 +0200

    commit (reestricturación + correcion archivos estáticos)

---
+++ DjangoGestionProyectos/static/expandirInfo.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Capturar el evento de clic en el botón para expandir información
+  document
+    .getElementById("expand-button")
+    .addEventListener("click", function () {
+      var infoBlock = document.getElementById("info-block");
+
+      if (infoBlock.style.display === "none") {
+        infoBlock.style.display = "block"; // Mostrar el bloque
+        this.textContent = "Ocultar información"; // Cambiar el texto del botón
+      } else {
+        infoBlock.style.display = "none"; // Ocultar el bloque
+        this.textContent = "Expandir información"; // Cambiar el texto del botón
+      }
+    });
+
+  // Capturar el evento de clic en el botón para mostrar alerta
+  document
+    .getElementById("alert-button")
+    .addEventListener("click", function () {
+      alert("Esta es una alerta de prueba.");
+    });
+});
